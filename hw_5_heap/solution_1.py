474f3a08456eaa55cbae5eb3c2455e04f7c03650
lanya-dorkin <danya.lorkin@gmail.com>
2023-12-25 21:54:19 +0300

    optimize solution_1.py

--- hw_5_heap/solution_1.py
+++ hw_5_heap/solution_1.py
@@ -1,28 +1,15 @@
-def heapify(arr: list, i: int):
-    l = 2 * i + 1
-    r = l + 1
-    smallest = i
-    if l < len(arr) and arr[l] < arr[smallest]: 
-        smallest = l
-    if r < len(arr) and arr[r] < arr[smallest]: 
-        smallest = r
-    if smallest != i:
-        arr[smallest], arr[i] = arr[i], arr[smallest]  
-        heapify(arr, smallest)
+from heapq import heappop, heappush
 
 
 def get_kth_element(arr: list, k: int):
-    # return sorted(arr)[k] # lame как-то что ли
     
-    for i in range(len(arr) // 2 - 1, -1, -1):
-        heapify(arr, i)
+    heap = []
+    for item in arr:
+        heappush(heap, -item)
+        if len(heap) > k + 1:
+            heappop(heap)
 
-    for _ in range(k):
-        arr[0], arr[-1] = arr[-1], arr[0]
-        arr.pop()
-        heapify(arr, 0)
-    
-    return arr[0]
+    return -heap[0]
 
 
 def solution():
